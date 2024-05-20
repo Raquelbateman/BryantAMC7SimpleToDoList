28058950d6083f26634fc59fd4911a1615bcd921
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-19 21:25:03 -0700

    update

--- simpleToDo/src/Components/ToDoList/ToDoList.tsx
+++ simpleToDo/src/Components/ToDoList/ToDoList.tsx
@@ -1,36 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const ToDoList = () => {
   return (
     <>
-
-<div className="myContainer">
-<div className="row">
-<h1>ToDo List</h1>
-<input type="text" value={} onChange={}/>
-<button className="addButton"> </button>
-
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-</div>
-
-
-
-
-
+      <div className="myContainer">
+        <div className="row">
+          <h1>ToDo List</h1>
+          <input type="text" value={} onChange={} />
+          <button className="addButton"> </button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
